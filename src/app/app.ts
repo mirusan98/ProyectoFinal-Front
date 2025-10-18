609f73c990c8f357b1ddd780c36c1922dee3aea8
mirusan98 <mirusan98@gmail.com>
2025-10-18 15:48:13 -0300

    Ejercicio 6

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,12 +4,13 @@ import { MensajeComponent } from './mensaje/mensaje';
 import { ObjetoComponent } from './objeto/objeto';
 import { ArregloComponent } from './arreglo/arreglo';
 import { HeaderComponent } from './header/header';
+import { FooterComponent } from './footer/footer';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  imports: [RouterOutlet, MensajeComponent, ObjetoComponent, ArregloComponent, HeaderComponent],
+  imports: [RouterOutlet, MensajeComponent, ObjetoComponent, ArregloComponent, HeaderComponent, FooterComponent],
 })
 export class App {
   protected readonly title = signal('ProyectoFinal');
---
+++ src/app/footer/footer.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './footer.html',
+  styleUrl: './footer.css',
+  standalone: true,
+})
+export class FooterComponent {
+mensajeFooter = 'Mi Página Angular - Curso FullStack UTN-GSC';
+}
